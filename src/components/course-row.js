faac8f00932167d5a141884127191d35355d1917
yufeng-gao <georgegao3g@hotmail.com>
2021-02-22 12:42:58 -0500

    Implemented course table view and table rows.

---
+++ src/components/course-row.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+
+const CourseRow = ({
+    deleteCourse,
+    updateCourse,
+    course,
+    lastModified,
+    title,
+    owner
+}) => {
+    const [editing, setEditing] = useState(false)
+    const [newTitle, setNewTitle] = useState(title)
+
+    const saveTitle = () => {
+        setEditing(false)
+        const newCourse = {
+            ...course,
+            title: newTitle
+        }
+        updateCourse(newCourse)
+    }
+
+    return (
+        <tr>
+        <td>
+            {!editing &&
+                <Link to="/courses/editor">
+                    {title}
+                </Link>
+            }
+            {editing &&
+                <input
+                    onChange={(event) => setNewTitle(event.target.value)}
+                    value={newTitle}
+                    className="form-control"/>
+            }
+        </td>
+        <td className="d-none d-md-table-cell">{owner}</td>
+        <td className="d-none d-lg-table-cell">{lastModified}</td>
+        <td>
+            <i onClick={() => deleteCourse(course)} className="fas fa-trash"></i>
+            {!editing && <i onClick={() => setEditing(true)} className="fas fa-edit"></i>}
+            {editing && <i onClick={() => saveTitle()} className="fas fa-check"></i>}
+        </td>
+      </tr>
+    )
+}
+
+export default CourseRow;
--- src/components/course-table.js
+++ src/components/course-table.js
@@ -1,4 +1,4 @@
-//citation: 
+//citation: used the same CourseRow definition as the one in jannunzi repo
 import React from 'react'
 import CourseRow from "./course-row";
 import {Link} from "react-router-dom";
@@ -10,5 +10,39 @@ export default class CourseTable
             super(props);
         }
 
+    render() {
+        return (
+            <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <td>Title</td>
+                            <td className="d-none d-md-table-cell">Owned by</td>
+                            <td className="d-none d-lg-table-cell">Last modified</td>
+                            <td>
+                            <i className="fas fa-2x fa-folder"></i>
+                            <i className="fas fa-2x fa-sort-alpha-up"></i>
+                            <Link to="/courses/grid">
+                                <i className="fas fa-2x fa-th"></i>
+                            </Link>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.courses.map(course =>
+                            <CourseRow
+                                key={course._id}
+                                deleteCourse={this.props.deleteCourse}
+                                updateCourse={this.props.updateCourse}
+                                course={course}
+                                title={course.title}
+                                lastModified={course.lastModified}
+                                owner={course.owner} />)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 
     }
