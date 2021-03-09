81373dbb7f08af3a0791241349387bd6a5c9e56c
yufeng-gao <georgegao3g@hotmail.com>
2021-03-09 13:46:53 -0500

    Implemented moduleList and moduleReducer

---
+++ src/components/course-editor/module-list.js
@@ -0,0 +1,77 @@
+//citation: reused the code from jannunzi git repo
+import React, {useEffect} from 'react'
+import {connect, Provider} from "react-redux";
+import EditableItem from "./editable-item";
+import {useParams} from "react-router-dom";
+import moduleService from "../services/module-service"
+
+const ModuleList = (
+    {
+        modules = [],
+        createModule,
+        updateModule,
+        deleteModule,
+        findModulesForCourse
+    }) => {
+    const { layout, courseId, moduleId } = useParams();
+    useEffect(() => {
+        findModulesForCourse(courseId)
+    }, [])
+    return (
+        <div>
+            <h2>Module List</h2>
+            <ul>
+                <li>layout: {layout}</li>
+                <li>courseId: {courseId}</li>
+                <li>moduleId: {moduleId}</li>
+            </ul>
+
+
+            <ul className="list-group">
+                {
+                    modules.map(module =>
+                        <li className="list-group-item">
+                            <EditableItem
+                                to={`/courses/${layout}/edit/${courseId}/modules/${module._id}`}
+                                deleteItem={deleteModule}
+                                updateItem={updateModule}
+                                item={module} />
+                        </li>
+                    )
+                }
+                <li className="list-group-item">
+                    <i onClick={() => createModule(courseId)} className="fas fa-plus fa-2x"></i>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+const stpm = (state) => ({
+    modules: state.moduleReducer.modules
+})
+
+const dtpm = (dispatch) => ({
+    createModule: (courseId) => {
+        moduleService.createModule(courseId, {title: 'New Module'})
+            .then(module => dispatch({type: "CREATE_MODULE", module: module}))
+        
+    },
+    updateModule: (newItem) => {
+        moduleService.updateModule(newItem._id, newItem)
+            .then(status => dispatch({type: "UPDATE_MODULE", updateModule: newItem}))
+    },
+    deleteModule: (moduleToDelete) => {
+        moduleService.deleteModule(moduleToDelete._id)
+            .then(status => dispatch({type: "DELETE_MODULE", moduleToDelete: moduleToDelete}))
+    },
+    findModulesForCourse: (courseId) => {
+        moduleService.findModulesForCourse(courseId)
+            .then(modules => dispatch({
+                type: "FIND_MODULES_FOR_COURSE",
+                modules: modules  
+        }))
+    }
+})
+
+export default connect(stpm, dtpm)(ModuleList)
---
+++ src/reducers/module-reducer.js
@@ -0,0 +1,52 @@
+//citation: code from jannuzi git repo.
+const initialState = {
+    modules: [
+        {title: 'placeholder', _id: '123'},
+        {title: 'fake module', _id: '234'}
+    ],
+    other: 111
+}
+
+const moduleReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "CREATE_MODULE":
+            return {
+                ...state,
+                modules: [
+                    ...state.modules,
+                    action.module
+                ]
+            }
+        case "DELETE_MODULE":
+            return {
+                ...state,
+                modules: state.modules.filter(module => {
+                    if (module._id !== action.moduleToDelete._id) {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+            }
+        case "UPDATE_MODULE":
+            return {
+                ...state,
+                modules: state.modules.map(module => {
+                    if (module._id === action.updateModule._id) {
+                        return action.updateModule
+                    } else {
+                        return module
+                    }
+                })
+            }
+        case "FIND_MODULES_FOR_COURSE":
+            return {
+                ...state,
+                modules: action.modules
+            }
+        default:
+            return state
+    }
+}
+
+export default moduleReducer
